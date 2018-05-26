46933426333586cc2c0cd3084232d57f50133650
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2018-05-26 13:44:54 -0500

    finding a day of the week and hour prior to a certain time

--- linkgeneration/linkgeneration.py
+++ linkgeneration/linkgeneration.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta
 
 DEFAULT_WEEKS_BACK = 5
 HOURS_PER_DAY=24
+NUMBER_OF_DAYS_IN_WEEK = 7
 
 def weeks_or_days_date_back(
         num_days_weeks, most_recent_date,
@@ -42,7 +43,61 @@ def dates_with_included_start_and_last_hours(
           for hour_delta in hour_deltas ]
         for relevant_date in dates_to_work_through
         ]
+
+weekday_map = {
+    0: 'monday',
+    1: 'tuesday',
+    2: 'wednesday',
+    2: 'thursday',
+    2: 'friday',
+    5: 'saturday',
+    6: 'sunday',
+    }
+
+weekday_keys = list(weekday_map.keys())
+min_weekday = min(weekday_keys)
+max_weekday = max(weekday_keys)
+
+def valid_weekday(weekday):
+    return min_weekday<=weekday<=max_weekday
+
+def most_recent_date_w_day_of_week_prior_to(
+        weekday, start_hour, datetime_to_compare):
+    if not valid_weekday(weekday):
+        raise ValueError("not a valid week day")
     
+    recent_day = datetime_to_compare.weekday()
+    # I originally used iteration to find the most recent date
+    # with a matching day of the week
+    #for i in range(NUMBER_OF_DAYS_IN_WEEK):
+    #    if recent_day == weekday:
+    #        break
+
+        # we can use modular arithmatic if these assumptions hold
+    #    assert(min_weekday==0)
+    #    assert(max_weekday==6)
+    #    recent_day = (recent_day-1) % NUMBER_OF_DAYS_IN_WEEK
+    #found_date_near_comparetime = datetime_to_compare + timedelta(days=-i)
+
+    # but then I found I could do it arithmetically
+    num_days_prior = (recent_day - weekday) % NUMBER_OF_DAYS_IN_WEEK
+    found_date_near_comparetime = \
+        datetime_to_compare + timedelta(days=-num_days_prior)
+    
+    # use the provided start_hour
+    found_datetime = datetime( year=found_date_near_comparetime.year,
+                               month=found_date_near_comparetime.month,
+                               day=found_date_near_comparetime.day,
+                               hour=start_hour )
+    assert( found_datetime.weekday() == weekday )
+
+    if found_datetime <= datetime_to_compare:
+        return found_datetime
+    else:
+        return_datetime =  found_datetime - timedelta(7)
+        assert( return_datetime < datetime_to_compare)
+        return return_datetime
+
 if __name__ == "__main__":
     print( weeks_or_days_date_back(5, date.today() ) )
     print( weeks_or_days_date_back(20, date.today(), 
@@ -62,3 +117,8 @@ if __name__ == "__main__":
         weeks_or_days_date_back(5, date.today()),
         23,2
     ) )
+
+    print(
+        most_recent_date_w_day_of_week_prior_to(
+            6, 10, datetime(2018,5,26, 9)
+        ) )
