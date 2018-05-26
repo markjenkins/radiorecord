c4262ab25bb1bd6eace41378bcaf032670daeb67
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2018-05-26 15:56:21 -0500

    links formated as per CKUW 128kbit/s API

---
+++ linkgeneration/ckuw.py
@@ -0,0 +1,27 @@
+from .dategeneration import generate_dates_for_showtime
+
+URLBASE = "http://ckuw.ca/128/"
+#20180523.08.00-09.00.mp3
+FILESUFFIX = ".mp3"
+
+def generate_links_for_showtime(
+    dayofweek, starttimeofday, lasthour=None, weeksback=None):
+    if lasthour==None:
+        lasthour=starttimeofday
+    return [
+        [ (URLBASE +
+           "%(year).4d" + "%(month).2d" + "%(day).2d" + "." +
+           "%(starthour).2d.00-" +
+           "%(endhour).2d.00" + FILESUFFIX ) % {
+               'urlbase': URLBASE,
+               'year': ephour.year,
+               'month': ephour.month,
+               'day': ephour.day,
+               'starthour': ephour.hour,
+               'endhour': (lasthour+1) % 24
+               } # end dict fed to string formating
+          for ephour in episode] # end inner list comprehension 
+        for episode in generate_dates_for_showtime(
+                dayofweek, starttimeofday, lasthour=None, weeksback=None)
+        ] # end outer list comprehension
+
