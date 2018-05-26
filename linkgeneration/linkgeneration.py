3f30598d5d15c9965f292781fcd5e1bd8a9ce319
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2018-05-26 12:36:37 -0500

    time between a start and end hour
    for example (2,4) is 3 because it means the hour that starts at 2am, 3am, 4am
    and (23,2) is 4 hours starting at 23:00, 00:00, 01:00, 02:00

--- linkgeneration/linkgeneration.py
+++ linkgeneration/linkgeneration.py
@@ -10,9 +10,25 @@ def weeks_or_days_date_back(
         for i in range(num_days_weeks)
         ]
 
+HOURS_PER_DAY=24
+
+def valid_hour(hour):
+    return 0<=hour<=(HOURS_PER_DAY-1) # (0-23 inclusive)
+
+def hours_between_start_and_end_inclusive(start_hour, last_hour):
+    if not (valid_hour(start_hour) and valid_hour(last_hour)):
+        raise ValueError("invalid start or end hour")
+    
+    return (last_hour - start_hour) % 24 + 1
+
 DEFAULT_WEEKS_BACK = 5
 
 if __name__ == "__main__":
     print( weeks_or_days_date_back(5, date.today() ) )
     print( weeks_or_days_date_back(20, date.today(), 
                                    increment=timedelta(days=1) ) )
+    
+    print(hours_between_start_and_end_inclusive(6,8) )
+    print(hours_between_start_and_end_inclusive(23,4) )
+    print(hours_between_start_and_end_inclusive(23,0) )
+    print(hours_between_start_and_end_inclusive(10,4) )
