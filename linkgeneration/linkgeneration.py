3aadbab2c04ff26023204323740d325bc31d5f34
Mark Jenkins <ma.jenkins@uwinnipeg.ca>
2018-05-26 12:18:44 -0500

    initial code able to count back days or weeks

---
+++ linkgeneration/linkgeneration.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from datetime import date, timedelta
+
+def weeks_or_days_date_back(
+        num_days_weeks, most_recent_date,
+        increment=timedelta(days=7) ):
+    return [
+        most_recent_date - increment*i
+        for i in range(num_days_weeks)
+        ]
+
+DEFAULT_WEEKS_BACK = 5
+
+if __name__ == "__main__":
+    print( weeks_or_days_date_back(5, date.today() ) )
